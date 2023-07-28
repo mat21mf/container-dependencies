def1569dec3c7fcbee5b1defebf29b91fb680ab1
mat21mf <matias.rebolledo@gmail.com>
2023-07-28 14:21:58 -0400

    Modificar scripts.

--- call_dependencias_singularity.sh
+++ call_dependencias_singularity.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
-singularity exec /mnt/disk02/_singularity/rocker_shiny Rscript --vanilla verificar_dependencias.r
+# echo
+# echo "# Singularity Sandbox rocker_shiny"
+# echo
+# singularity exec /mnt/disk02/_singularity/rocker_shiny Rscript --vanilla verificar_dependencias.r
+
+echo
+echo "# Singularity Sandbox rocker_shiny_verse"
+echo
+singularity exec /mnt/disk02/_singularity/rocker_shiny_verse Rscript --vanilla verificar_dependencias.r
+
+echo
+echo "# Singularity Sandbox rocker_geospatial"
+echo
 singularity exec /mnt/disk02/_singularity/rocker_geospatial Rscript --vanilla verificar_dependencias.r
--- verificar_dependencias.r
+++ verificar_dependencias.r
@@ -1,7 +1,11 @@
+#!/usr/bin/env R
+
 str_repos <- "https://cran.r-project.org"
 if(suppressPackageStartupMessages(!require(data.table))) {install.packages("data.table" , repos = str_repos )}
 suppressPackageStartupMessages(library(data.table))
 data.table::update_dev_pkg()
+suppressPackageStartupMessages(library(data.table))
+cat('\n')
 
 # provisional
 vec_dependencias <- c( "DT", "arrow", "data.table", "dplyr", "ggplot2",
@@ -21,3 +25,4 @@ obj_outer <- obj_interseccion[obj_requeridos, on=.(Package) ]
 obj_outer[, i.Estado := ifelse( !is.na(Version), TRUE, FALSE ) ]
 obj_outer[, Estado := NULL ]
 print(obj_outer)
+cat('\n')
